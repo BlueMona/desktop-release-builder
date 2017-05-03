656a83eaa89b5346f2d68c8abf72e38cde5db49e
Dmitry Chestnykh <dmitry@codingrobots.com>
2017-05-03 20:19:42 +0200

    feat: allow launching override from command line

--- override.js
+++ override.js
@@ -4,6 +4,26 @@ const { execSync } = require('child_process');
 const merge = require('lodash/merge');
 const { ncp } = require('ncp');
 
+if (require.main === module) {
+    // Launched as an executable.
+
+    // Check command-line arguments.
+    if (process.argv.length < 3) {
+        console.error('Usage: node override OVERRIDES_DIR DEST_DIR');
+        process.exit(1);
+    }
+
+    const SRC_DIR = process.argv[2];
+    const DST_DIR = process.argv[3];
+
+    override(SRC_DIR, DST_DIR)
+        .then(() => process.exit(0))
+        .catch(err => {
+            console.error(err);
+            process.exit(1);
+        });
+}
+
 function override(srcDir, dstDir) {
     // Merge json overrides.
     let jsonOverridesPath = path.join(srcDir, 'json-overrides.json');
